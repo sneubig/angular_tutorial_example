3fb6d3056b44f51cd37694d5d7217f1457770ea9
sneubig <neubig.stephen@gmail.com>
2014-12-02 12:52:42 -0500

    added controller

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-var app = angular.module('store', []);
+(function(){
 
+	var app = angular.module('store', []);
+
+	// The controller is attached to (inside) the app
+	app.controller('StoreController', function(){
+		this.product = gem;
+	});
+
+	var gem = {
+		name: 'Dodecahedron', 
+		price: 2.95,
+		description: '...',
+	}
+})();
